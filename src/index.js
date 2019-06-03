bf9363e4861e626c07f4c9186839c4f3cd9d5627
Dariusz Syncerek <dsyncerek@outlook.com>
2019-06-03 16:22:18 +0200

    Add styled-components (global styles and variables)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './app/styles/variables.styled';
+import { GlobalStyled } from './app/styles/global.styled';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyled />
+      <App />
+    </>
+  </ThemeProvider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
