cfa37aaefd8fe5ce7ec9b0f637e26ecfa91672b3
Dariusz Syncerek <dsyncerek@outlook.com>
2019-06-07 13:52:34 +0200

    Add session provider

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from './app/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './app/styles/variables.styled';
 import { GlobalStyled } from './app/styles/global.styled';
+import { SessionContextProvider } from './app/context/SessionContext';
 
 const Root = () => (
-  <ThemeProvider theme={theme}>
-    <>
-      <GlobalStyled />
-      <App />
-    </>
-  </ThemeProvider>
+  <SessionContextProvider>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyled />
+        <App />
+      </>
+    </ThemeProvider>
+  </SessionContextProvider>
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
