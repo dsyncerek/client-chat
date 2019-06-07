55a1c43f8f07ea15d7a1843901521d80232e7d07
Dariusz Syncerek <dsyncerek@outlook.com>
2019-06-07 17:54:17 +0200

    Add redirection to /client if 404

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import HostChat from './pages/HostChat';
 import Sessions from './pages/Sessions';
 import ClientChat from './pages/ClientChat';
@@ -7,9 +7,12 @@ import ClientChat from './pages/ClientChat';
 const App = () => {
   return (
     <Router>
-      <Route exact path="/host/:session" component={HostChat} />
-      <Route exact path="/host" component={Sessions} />
-      <Route exact path="/client" component={ClientChat} />
+      <Switch>
+        <Route exact path="/host/:session" component={HostChat} />
+        <Route exact path="/host" component={Sessions} />
+        <Route exact path="/client" component={ClientChat} />
+        <Redirect to="/client" />
+      </Switch>
     </Router>
   );
 };
