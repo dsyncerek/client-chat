16419e8de6b580138b7bb6e457dc087bfff2ae3c
Dariusz Syncerek <dsyncerek@outlook.com>
2019-06-07 15:06:53 +0200

    Make chat auto-scrollable

--- src/app/components/Chat/Chat.styled.js
+++ src/app/components/Chat/Chat.styled.js
@@ -1,5 +1,22 @@
 import styled, { css } from 'styled-components';
+import { SendMessageStyled } from '../SendMessage/SendMessage.styled';
+import { MessageListStyled } from '../MessageList/MessageList.styled';
 
 export const ChatStyled = styled.div(props => css`
+  position: relative;
+  height: 100vh;
+  padding-bottom: 62px;
   
+  ${MessageListStyled} {
+    position: relative;
+    overflow: auto;
+    height: 100%;
+  }
+
+  ${SendMessageStyled} {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
 `);
